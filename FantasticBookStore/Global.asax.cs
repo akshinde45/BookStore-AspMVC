a9287a193cfec470254e1a0dbdc8dd3660e05557
akshinde45 <akshayshinde45.as@gmail.com>
2020-04-04 17:42:56 +0530

    commit

--- FantasticBookStore/Global.asax.cs
+++ FantasticBookStore/Global.asax.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 
 namespace FantasticBookStore
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public   class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
